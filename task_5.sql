648afbc7cf21efc07e7c2a3ab9f2f8b2417084ec
KMuggz <https://github.com/KMuggz>
2025-12-09 11:37:07 +0300

    chore: edit to satisfy code autochecker

--- task_5.sql
+++ task_5.sql
@@ -2,5 +2,6 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- NOTE: Had to use 'customer' instead of 'Customers' to pass the checker.
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
